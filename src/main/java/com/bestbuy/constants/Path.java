928df58761c7d8c9ef6dd4bc6d839d55efd9528b
pinal882 <pinal.shah882@gmail.com>
2024-01-23 10:28:36 +0000

    my homework

---
+++ src/main/java/com/bestbuy/constants/Path.java
@@ -0,0 +1,11 @@
+package com.bestbuy.constants;
+
+public class Path {
+    /*
+     * This is base path for store  api
+     */
+    public static final String STORES = "/stores";
+    //This path for products API
+    public static final String PRODUCTS = "/products";
+
+}
